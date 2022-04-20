73dab2c34b12860caf39237008afa94fb18d61c6
SpiderDerp <62308557+SpiderDerp@users.noreply.github.com>
2022-04-20 11:22:01 -0400

    information

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'NiceEncryption',
+    packages = ['NiceEncryption'],
+    version = 'NiceEncryption',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A Python package that encrypts and decrypts strings.',
+    author = 'SpiderDerp',
+    author_email = '',
+    url = 'https://github.com/SpiderDerp/NiceEncryption',
+    download_url = 'https://github.com/SpiderDerp/NiceEncryption/archive/refs/tags/0.1.tar.gz',
+    keywords = ['python', 'encryption'],
+    classifiers = [],
+)
